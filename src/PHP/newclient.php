193954d5ce14578c9cac3aa38d38a3247eb3c643
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-03-28 13:21:25 +0200

    Début de modélisation de la navigation

--- src/PHP/newclient.php
+++ src/PHP/newclient.php
@@ -15,33 +15,41 @@
 			$res_Villes = pg_exec($connectDB,$req_Villes);
 
 			// Saisie des informations
-			echo "
+			echo '
 				<h3>Toutes les informations sont obligatoires</h3>
-				<form name=\"nouveauClient\" method=\"post\" action=\"newclient.php\">
-					Nom : 			<input type=\"text\" name=\"nom\"/> <br/>
-					Adresse : 		<input type=\"text\" name=\"adresse\"/><br/>
-					Code postal : 	<input type=\"text\" name=\"cp\"/>
-					<input type=\"submit\" name=\"vilCherch\" value=\"Chercher les villes\"/><br/>
-					";
+				<form name="nouveauClient" method="post" action="newclient.php">
+					Nom : 			<input type="text" name="nom"/> <br/>
+					Adresse : 		<input type="text" name="adresse"/><br/>
+					Code postal : 	<input type="text" name="cp"/>
+					<input type="submit" name="vilCherch" value="Chercher les villes"/><br/>
+					';
 				if(isset($_POST["vilCherch"]))
 				{
 					$cp_c=$_POST['cp'];
-					// echo "Appui chercher villes $cp_c";
+					echo "Appui chercher villes $cp_c";
+/*
 					$villesQuery="select num_v, nom_v from villes where code_postal_v='$cp_c' ORDER by nom_v ;";
 					$listVilles	= pg_exec($connectDB,$villesQuery);
 					$nbVilles 	= pg_num_rows($listVilles);
+
+					echo "nbVilles $nbVilles";
+					* */
 				};
 
-				echo "Ville : <select name=\"ville\"> ";
+				// echo "Ville : <select name=\"ville\"> ";
+				echo '<option value="-1">Choisissez la ville</option>';
 				for($li=0; $li < $nbVilles; $li++) 
+				// while($row = pg_fetch_assoc($villesQuery))
 				{
 					$row = pg_fetch_array($listVilles, $li);
-					echo("<option value="+$row["num_v"]+">"+$row["nom_v"]+"</option>");
+					// echo("<option value="+$row["num_v"]+">"+$row["nom_v"]+"</option>");
+					echo '<option value="'.$row["num_v"].'">'.$row["nom_v"].'</option>';
 				} ;
-			echo "	</select><br/>
-					Téléphone : 	<input type=\"text\" name=\"tel\"/><br/>
-					<input type=\"submit\" name=\"valider\" value=\"Valider\"/>
-				</form>";
+			
+			echo '	</select><br/>
+					Téléphone : 	<input type="text" name="tel"/><br/>
+					<input type="submit" name="valider" value="Valider"/>
+				</form>';
 		
 		?>
 		<!-- Traitement après clic sur le bouton ok-->
