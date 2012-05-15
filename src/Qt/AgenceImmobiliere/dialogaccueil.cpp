61837af7c7a256432554a86aef1241724e87314e
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-05-15 14:05:21 +0200

    Mise à niveau de la barre de menu de l'écran d'accueil Ajout de nouvelles ressources

--- src/Qt/AgenceImmobiliere/dialogaccueil.cpp
+++ src/Qt/AgenceImmobiliere/dialogaccueil.cpp
@@ -8,6 +8,10 @@ DialogAccueil::DialogAccueil(QWidget *parent) :
     ui->setupUi(this);
     m_menu.setupUi(ui->widget_barreMenu);
     m_menu.label_fenetre->setText("Accueil");
+    m_menu.image_fenetre->setPixmap(QPixmap(":/app/accueil"));
+    m_menu.pushButton_3->setVisible(false);
+    m_menu.pushButton_1->setIcon(QIcon(":/app/rafraichir96"));
+    m_menu.pushButton_2->setIcon(QIcon(":/app/quitter96"));
 }
 
 DialogAccueil::~DialogAccueil()
