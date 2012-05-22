1c0459a2580db5a925d24b007a6036a5bc680290
Gilles <gilles.coulais@etu.u-bordeaux1.fr>
2012-05-22 18:28:38 +0200

    Création du fichier de configuration Doxygen et création de la doc de la classe Client

--- src/Qt/AgenceImmobiliere/Client.h
+++ src/Qt/AgenceImmobiliere/Client.h
@@ -6,23 +6,120 @@
 #include <Ville.h>
 #include <BDD.h>
 
+/*!
+  \file Client.h
+  \brief Client permet de décrire un client. Il permet la communication avec
+  \author Gilles Coulais
+  \version 1.0
+  */
+
 class Client : public QObject
 {
 
 public:
+    /*!
+      \brief    Constructeur
+      \param [in]   num entier, le numéro du client
+      \param [in]   nom QString, le nom du client
+      \param [in]   adresse QString, l'adresse du client
+      \param [in]   tel QString, le téléphone du client
+      \param [in,out]   ville  Ville *, la ville du client
+      \param [in]   numA entier, le numéro d'agent affecté au client acheteur
+      */
     Client (int num, QString nom,QString adresse,QString tel, Ville *ville, int numA);
+
+    /*!
+      \brief    Destructeur
+      */
     ~Client ();
+
+    /*!
+      \brief    Modifier l'adresse
+      Permet de modifier l'adresse
+      \param [in]   adr QString, la nouvelle adresse
+      \return   void
+      */
     void setAdresse (QString adr);
+
+    /*!
+      \brief    Modifier le téléphone
+      Permet de modifier le téléphone
+      \param [in]   tel QString, le nouveau téléphone
+      \return   void
+      */
     void setTelephone (QString tel);
+
+    /*!
+      \brief    Modifier le nom
+      Permet de modifier le nom
+      \param [in]   nom QString, le nouveau nom
+      \return   void
+      */
     void setNom(QString nom);
+
+    /*!
+      \brief    Modifier la ville
+      Permet de modifier la ville
+      \param [in,out]   ville Ville *, la nouvelle ville
+      \return   void
+      */
     void setVille(Ville *ville);
+
+    /*!
+      \brief    Modifier le numéro d'agent
+      Permet de modifier le numéro d'agent
+      \param [in]   num int, le nouveau numéro
+      \return   void
+      */
     void setNumA(int num);
+
+    /*!
+      \brief    Obtenir le numéro client
+      Permet d'obtenir le numéro du client
+      \return   int le numéro du client
+      */
     int getNum();
+
+    /*!
+      \brief    Obtenir le numéro d'agent
+      Permet d'obtenir le numéro de l'agent affecté au client
+      \return   int le numéro de l'agent
+      */
     int getNumA();
+
+    /*!
+      \brief    Obtenir le nom
+      Permet d'obtenir le nom du client
+      \return   QString le nom du client
+      */
     QString getNom();
+
+    /*!
+      \brief    Obtenir l'adresse
+      Permet d'obtenir l'adresse du client
+      \return   QString l'adresse du client
+      */
     QString getAdresse();
+
+    /*!
+      \brief    Obtenir le téléphone
+      Permet d'obtenir le téléphone du client
+      \return   QString le téléphone du client
+      */
     QString getTel();
+
+    /*!
+      \brief    Obtenir la ville
+      Permet d'obtenir la ville du client
+      \return   Ville*  la ville du client
+      */
     Ville* getVille();
+
+    /*!
+      \brief    Sauvegarder le client
+      Permet de sauvegarder le client dans la base de données. Si le client n'existe pas dans la base, il est alors créé.
+      \return   bool    vrai si la sauvegarde s'est correctement déroulée
+      */
     bool sauvegarder();
 
 private:
